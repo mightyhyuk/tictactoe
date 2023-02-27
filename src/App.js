b4b88446fe50fc9daa3bec616bf4ac04f16d1638
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-02-27 21:51:11 +0900

    Take turns: each time a player moves

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ function Square({ value, handleSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleSquareClick(i) {
     const nextSquares = [...squares];
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
