2409033d3dec3b8cba3b2f55d4c3e0d57328d4a1
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-03-01 22:26:14 +0900

    Lift state up from Board into Game component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, handleSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -80,6 +80,19 @@ export default function Board() {
         />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
